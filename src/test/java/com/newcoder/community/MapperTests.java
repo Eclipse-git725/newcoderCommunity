ffee39381148f04c9e22b8419c673e759c68050c
gxy <2219570448@qq.com>
2024-05-11 14:26:51 +0800

    Mybatis 前置知识

---
+++ src/test/java/com/newcoder/community/MapperTests.java
@@ -0,0 +1,60 @@
+package com.newcoder.community;
+
+import com.newcoder.community.dao.AlphaDao;
+import com.newcoder.community.dao.UserMapper;
+import com.newcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("https://www.newcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        int rows = userMapper.updateStatus(1924665351, 1);
+        System.out.println(rows);
+        rows = userMapper.updateHeader(1924665351,"http://images.nowcoder.com/head/120t.png");
+        System.out.println(rows);
+        rows = userMapper.updatePassword(1924665351,"123456");
+        System.out.println(rows);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        System.out.println(userMapper.deleteByName("test"));
+    }
+}
